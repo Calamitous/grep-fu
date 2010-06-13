2328da74ad8eddab61bc0d3fda17f97b545cd0ad
Eric Budd <eric.b.budd@gmail.com>
2010-06-13 15:21:16 -0500

    Wrapped Options class in grep-fu Module

--- lib/grep-fu/options.rb
+++ lib/grep-fu/options.rb
@@ -1,54 +1,56 @@
-class Options
-	attr_reader :verbose, :single_line, :color, :search_criteria, :file_path 
+module GrepFu
+	class Options
+		attr_reader :verbose, :single_line, :color, :search_criteria, :file_path 
 
-	PATH_REPLACEMENTS = {
-		'a' => 'app', 
-		'c' => 'app/controllers', 
-		'h' => 'app/helpers', 
-		'm' => 'app/models', 
-		'v' => 'app/views', 
-		'l' => 'lib',
-		'p' => 'public', 
-		'css' => 'public/stylesheets', 
-		'js' => 'public/javascripts', 
-		't' => 'test',
-		's' => 'spec',
-		'vp' => 'vendor/plugins',
-		'mig' => 'db/migrate'
-	}
+		PATH_REPLACEMENTS = {
+			'a' => 'app', 
+			'c' => 'app/controllers', 
+			'h' => 'app/helpers', 
+			'm' => 'app/models', 
+			'v' => 'app/views', 
+			'l' => 'lib',
+			'p' => 'public', 
+			'css' => 'public/stylesheets', 
+			'js' => 'public/javascripts', 
+			't' => 'test',
+			's' => 'spec',
+			'vp' => 'vendor/plugins',
+			'mig' => 'db/migrate'
+		}
 
-	COLOR_ON_BY_DEFAULT = false
+		COLOR_ON_BY_DEFAULT = false
 
-	def self.usage(file)
-		lines = ['',
-			"Usage: #{file} [findpath] search_string [--verbose|--single-line]\n",
+		def self.usage(file)
+			lines = ['',
+				"Usage: #{file} [findpath] search_string [--verbose|--single-line]\n",
 			"    Where findpath is one of the following:",
-			"      any literal subdirectory",
-			Options::PATH_REPLACEMENTS.map { |abbr, txt| "      #{abbr} - #{txt}" }.join("\n"),
+				"      any literal subdirectory",
+				Options::PATH_REPLACEMENTS.map { |abbr, txt| "      #{abbr} - #{txt}" }.join("\n"),
 			"\n\n"
-		]
+			]
 
-		lines.join("\n")
-	end
-
-	def initialize(args)
-		@verbose = (args -= ['--verbose'] if args.include?('--verbose'))
-		@single_line = (args -= ['--single-line'] if args.include?('--single-line'))
-
-		@color = (args -= ['--color'] if (args.include?('--color')) || COLOR_ON_BY_DEFAULT)
-		if args.include?('--no-color')
-			args -= ['--no-color']
-			@color = false
+			lines.join("\n")
 		end
 
-		@search_criteria = args.last
-		@file_path = args.size == 2 ? PATH_REPLACEMENTS[args.first] || args.first : './'
-	end
+		def initialize(args)
+			@verbose = (args -= ['--verbose'] if args.include?('--verbose'))
+			@single_line = (args -= ['--single-line'] if args.include?('--single-line'))
 
-	def to_s
-		options = @verbose ? '-rin' : '-ril'
-		options << ' --color=always' if @color
-		options
-	end
+			@color = (args -= ['--color'] if (args.include?('--color')) || COLOR_ON_BY_DEFAULT)
+			if args.include?('--no-color')
+				args -= ['--no-color']
+				@color = false
+			end
 
+			@search_criteria = args.last
+			@file_path = args.size == 2 ? PATH_REPLACEMENTS[args.first] || args.first : './'
+		end
+
+		def to_s
+			options = @verbose ? '-rin' : '-ril'
+			options << ' --color=always' if @color
+			options
+		end
+
+	end
 end
