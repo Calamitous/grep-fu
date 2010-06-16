3db8c0e5e8aa304ce9eb82c398b1e9f2cd01784f
Eric Budd <eric.b.budd@gmail.com>
2010-06-16 11:56:40 +0800

    Fixed stupid tabs because I forgot to :set et

--- lib/grep-fu/find_builder.rb
+++ lib/grep-fu/find_builder.rb
@@ -1,20 +1,20 @@
 module GrepFu
-	class FindBuilder
-		PRUNE_PATHS = ['/.svn', '/.git', '/vendor', '/log', '/public', '/tmp', '/coverage']
+  class FindBuilder
+    PRUNE_PATHS = ['/.svn', '/.git', '/vendor', '/log', '/public', '/tmp', '/coverage']
 
-		def self.delicious_prunes
-			"-path '*#{PRUNE_PATHS.join("' -prune -o -path '*")}' -prune -o"
-		end
+    def self.delicious_prunes
+      "-path '*#{PRUNE_PATHS.join("' -prune -o -path '*")}' -prune -o"
+    end
 
-		def self.find_command(options)
-			[
-				'find',
-				options.file_path,
-				delicious_prunes,
-				"\\( -size -100000c -type f \\) -print0 | xargs -0 grep",
-				options.to_s,
-				"\"#{options.search_criteria}\""
-			].join(' ')
-		end
-	end
+    def self.find_command(options)
+      [
+        'find',
+        options.file_path,
+        delicious_prunes,
+        "\\( -size -100000c -type f \\) -print0 | xargs -0 grep",
+        options.to_s,
+        "\"#{options.search_criteria}\""
+      ].join(' ')
+    end
+  end
 end
